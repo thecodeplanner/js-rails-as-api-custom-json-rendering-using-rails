bdca013a51af1a025020dda5e44475c3acfc1fa0
thecodeplanner <thecodeplanner@gmail.com>
2020-12-23 21:56:35 -0500

    commit tree 961926f5294df14f8e1a23519fb6cb51a74de25e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
   end
+
+  def show 
+    bird = Bird.find_by(id: params[:id])
+    render json:{id: bird.id, name: bird.name, species: bird.species } 
+  end
+
+
 end
